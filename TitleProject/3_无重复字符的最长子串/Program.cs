a5b7870458be4064759b3546feddb0f6fab240de
Xnco <xuanyylhky@163.com>
2018-11-03 16:51:58 +0800

    Add Title3
    3. 无重复字符的最长子串

---
+++ TitleProject/3_无重复字符的最长子串/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_无重复字符的最长子串
+{
+    /*
+        给定一个字符串，找出不含有重复字符的最长子串的长度。
+
+        示例 1:
+
+        输入: "abcabcbb"
+        输出: 3 
+        解释: 无重复字符的最长子串是 "abc"，其长度为 3。
+        示例 2:
+
+        输入: "bbbbb"
+        输出: 1
+        解释: 无重复字符的最长子串是 "b"，其长度为 1。
+        示例 3:
+
+        输入: "pwwkew"
+        输出: 3
+        解释: 无重复字符的最长子串是 "wke"，其长度为 3。
+        请注意，答案必须是一个子串，"pwke" 是一个子序列 而不是子串。
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int LengthOfLongestSubstring(string s)
+        {
+            int max = 0;
+            for (int 我 = 0; i < length; i++)
+            {
+
+            }
+
+            return max;
+        }
+    }
+
+        
+}
