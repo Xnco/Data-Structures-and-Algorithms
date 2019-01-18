37d09ab9f8841cd5a6641fcaa3166d66138b2bcd
Anthony_FU <xuanyylhky@163.com>
2019-01-18 17:55:50 +0800

    Add 104
    添加 104题 二叉树的最大深度

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
-# LeetCode-Project
-有时间刷一些LeetCode题库的中的题, 除了蛮力(暴力)算法外,给出一些其他更优解，或者做一些优化  
+# <span id='top'>Data-Structures-and-Algorithms</span>
+学习 数据结构 和 算法 的时候记录一些东西，锻炼下逻辑。
+闲暇时刷一些LeetCode题库的中的题，一般一道题或者某种算法会给出多种方案，并且分析利弊(时间/空间复杂度)。  
+会不断完善各式算法的问题，完成详情见 <a href="#directory">目录</a>，具体代码见工程/.cs文件。
+
 环境要求: 
 * [Visual Studio 2017](https://visualstudio.microsoft.com/)  
-* 或者直接随便用什么编辑器打开, Copy代码到 [LeetCode](https://leetcode-cn.com/problemset/all/) 中打开  
+* 或者直接随便用什么编辑器打开, Copy代码到 [LeetCode](https://leetcode-cn.com/problemset/all/) 中可提交运行
+* LeetCode上的编译事件仅供参考，同一段代码运行两次时间能相差 100ms，一次超过5%的人，一次超过60%的人。就是看心情的
 
-
-## 目前完成  
+## <span id="directory">目前完成目录</span>  
 * 1_两数之和_简单
 * 2_两数相加_中等
 * 3_无重复字符的最长子串_中等
 * 4_寻找两个有序数组的中位数_困难
 * 37_解数独_困难
+* 70_爬楼梯_简单
+* 104_二叉树的最大深度_简单
 * 633_平方数之和_简单
 * 771_宝石和石头_简单
 * 807_保持城市天际线_中等
@@ -18,3 +23,5 @@
 * 希尔排序
 * 归并排序
 * 快速排序
+
+<a href='#top'> 回到顶部</a>
---
+++ TitleProject/0104_二叉树的最大深度/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0104_二叉树的最大深度
+{
+
+    //Definition for a binary tree node.
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+ 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode tree1 = new TreeNode(3);
+            TreeNode tree2 = new TreeNode(9);
+            TreeNode tree3 = new TreeNode(20);
+            TreeNode tree4 = new TreeNode(15);
+            TreeNode tree5 = new TreeNode(7);
+
+            tree1.left = tree2;
+            tree1.right = tree3;
+            tree3.left = tree4;
+            tree3.right = tree5;
+
+            Console.WriteLine(MaxDepth_Iteration(tree1)); 
+        }
+
+        // 方案1 Leetcode上 最好成绩160ms
+        static int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            int maxDepth = 1;
+            int curDepth = 1;
+
+            AddDepth(root, ref maxDepth, ref curDepth);
+
+            return maxDepth;
+        }
+
+        static void AddDepth(TreeNode tree, ref int maxDepth, ref int curDepth)
+        {
+            if (tree.left != null)
+            {
+                curDepth++;
+                if (curDepth > maxDepth)
+                {
+                    maxDepth = curDepth;
+                }
+
+                AddDepth(tree.left, ref maxDepth, ref curDepth);
+                curDepth--;
+            }
+
+            if (tree.right != null)
+            {
+                curDepth++;
+                if (curDepth > maxDepth)
+                {
+                    maxDepth = curDepth;
+                }
+
+                AddDepth(tree.right, ref maxDepth, ref curDepth);
+                curDepth--;
+            }
+        }
+
+        // 方案2 最好成绩148ms，但有时会超时，不知道什么原因 
+        public int MaxDepth2(TreeNode root)
+        {
+            return root == null ? 0 : Math.Max(MaxDepth(root.left), MaxDepth(root.right)) + 1;
+        }
+
+        // 160ms， if和三元运算符感觉差不多的样子
+        static int MaxDepth3(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            else
+            {
+                int left_height = MaxDepth3(root.left);
+                int right_height = MaxDepth3(root.right);
+                return Math.Max(left_height, right_height) + 1;
+            }
+        }
+
+        // 迭代来算 -> 时间复杂度：O(N)  空间复杂度：O(N) 最好成绩156ms
+        static int MaxDepth_Iteration(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            Queue<KeyValuePair<TreeNode, int>> all = new Queue<KeyValuePair<TreeNode, int>>();
+            all.Enqueue(new KeyValuePair<TreeNode, int>(root, 1));
+
+            int depth = 1;
+            while (all.Count != 0)
+            {
+                var cur = all.Dequeue();
+                var curNode = cur.Key;
+                var curDepth = cur.Value;
+
+                // 当前节点不空
+                if (curNode != null)
+                {
+                    depth = Math.Max(depth, curDepth);
+                    all.Enqueue(new KeyValuePair<TreeNode, int>(curNode.left, curDepth + 1));
+                    all.Enqueue(new KeyValuePair<TreeNode, int>(curNode.right, curDepth + 1));
+                }
+            }
+
+            return depth;
+        }
+    }
+}
