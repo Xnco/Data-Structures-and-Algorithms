97016a4e6b17dda54af5a81538afe746fc145da7
Anthony_FU <xuanyylhky@163.com>
2019-06-10 10:46:17 +0800

    Add 141.cpp

---
+++ TitleProject_Cpp/0141_环形链表.cpp
@@ -0,0 +1,53 @@
+﻿
+#pragma region 141_环形链表
+/*
+给定一个链表，判断链表中是否有环。
+为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+示例 1：
+输入：head = [3,2,0,-4], pos = 1
+输出：true
+解释：链表中有一个环，其尾部连接到第二个节点。
+示例 2：
+输入：head = [1,2], pos = 0
+输出：true
+解释：链表中有一个环，其尾部连接到第一个节点。
+示例 3：
+输入：head = [1], pos = -1
+输出：false
+解释：链表中没有环。
+进阶：
+你能用 O(1)（即，常量）内存解决此问题吗？
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/linked-list-cycle
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	// 快慢指针 - 如果两个指针相遇, 则有环
+	// 8ms(99.12%), 9.9MB(20.4%)
+	bool hasCycle(ListNode* head) {
+		ListNode* first = head;
+		ListNode* second = head;
+
+		while (first != nullptr && second != nullptr && second->next != nullptr)
+		{
+			first = first->next;
+			second = second->next->next;
+			if (first == second)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+};
+#pragma endregion
