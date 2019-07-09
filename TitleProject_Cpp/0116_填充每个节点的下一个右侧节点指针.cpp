bbc437de3260d64106f1b427215c9e7ed9e2c98f
Anthony_FU <xuanyylhky@163.com>
2019-07-09 12:04:38 +0800

    Add 116.cpp

---
+++ TitleProject_Cpp/0116_填充每个节点的下一个右侧节点指针.cpp
@@ -0,0 +1,69 @@
+﻿
+#pragma region 116_填充每个节点的下一个右侧节点指针
+/*
+给定一个完美二叉树，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+初始状态下，所有 next 指针都被设置为 NULL。
+示例：
+输入：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":{"$id":"6","left":null,"next":null,"right":null,"val":6},"next":null,"right":{"$id":"7","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}
+输出：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":{"$id":"4","left":null,"next":{"$id":"5","left":null,"next":{"$id":"6","left":null,"next":null,"right":null,"val":7},"right":null,"val":6},"right":null,"val":5},"right":null,"val":4},"next":{"$id":"7","left":{"$ref":"5"},"next":null,"right":{"$ref":"6"},"val":3},"right":{"$ref":"4"},"val":2},"next":null,"right":{"$ref":"7"},"val":1}
+解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。
+提示：
+你只能使用常量级额外空间。
+使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+
+// Definition for a Node.
+class Node {
+public:
+	int val;
+	Node* left;
+	Node* right;
+	Node* next;
+
+	Node() {}
+
+	Node(int _val, Node* _left, Node* _right, Node* _next) {
+		val = _val;
+		left = _left;
+		right = _right;
+		next = _next;
+	}
+};
+
+class Solution {
+public:
+	// 递归 292ms(7.44%), 27.1MB(60.95%)
+	Node* connect(Node* root) {
+		bfs(root);
+		return root;
+	}
+
+	Node* bfs(Node* root)
+	{
+		if (root == nullptr || root->left == nullptr)
+		{
+			return nullptr;
+		}
+
+		root->left->next = root->right;
+		root->right->next = bfs(root->next);
+
+		bfs(root->left);
+		bfs(root->right);
+
+		return root->left;
+	}
+};
+#pragma endregion
