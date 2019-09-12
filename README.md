4987bd892db42c72a1cbdb12900f2cdb6a11c702
Anthony_FU <xuanyylhky@163.com>
2019-09-12 09:54:57 +0800

    Add 912.cpp

--- README.md
+++ README.md
@@ -180,6 +180,7 @@ ps : 目前题库为LeetCode，一般一道题会给出多种方案，并且分�
 * 876 - <font size=4>链表中的中间节点</font> - 简单
 * 881 - <font size=4>救生艇</font> - 中等
 * 905 - <font size=4>按奇偶排序数组</font> - 简单
+* 912 - <font size=4>排序数组</font> - 中等
 * 941 - <font size=4>有效的山脉数组</font> - 简单 - 2
 * 951 - <font size=4>翻转等价二叉树</font> - 中等
 * 961 - <font size=4>重复N次的元素</font> - 简单
---
+++ TitleProject_Cpp/0912_排序数组.cpp
@@ -0,0 +1,30 @@
+﻿
+#pragma region 912_排序数组
+/*
+给定一个整数数组 nums，将该数组升序排列。
+示例 1：
+输入：[5,2,3,1]
+输出：[1,2,3,5]
+示例 2：
+输入：[5,1,1,2,0,0]
+输出：[0,0,1,1,2,5]
+提示：
+1 <= A.length <= 10000
+-50000 <= A[i] <= 50000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/sort-an-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+class Solution {
+public:
+	// 不要重复造轮子
+	//Runtime: 52 ms, faster than 97.56% of C++ online submissions for Sort an Array.
+	//Memory Usage : 12.5 MB, less than 94.44 % of C++ online submissions for Sort an Array.
+	vector<int> sortArray(vector<int>& nums) {
+		sort(nums.begin(), nums.end());
+		return nums;
+	}
+};
+#pragma endregion
