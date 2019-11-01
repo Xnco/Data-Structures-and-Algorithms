705f7e964d1eca4e7260f335ac4baeb75fa12cfa
Anthony_FU <xuanyylhky@163.com>
2019-11-01 15:46:42 +0800

    Update TitleProject_Cpp.cpp

--- TitleProject_Cpp/TitleProject_Cpp/TitleProject_Cpp.cpp
+++ TitleProject_Cpp/TitleProject_Cpp/TitleProject_Cpp.cpp
@@ -23,6 +23,34 @@ using namespace std;
 */
 #pragma endregion
 
+#pragma region 858_镜面反射
+/*
+有一个特殊的正方形房间，每面墙上都有一面镜子。除西南角以外，每个角落都放有一个接受器，编号为 0， 1，以及 2。
+正方形房间的墙壁长度为 p，一束激光从西南角射出，首先会与东墙相遇，入射点到接收器 0 的距离为 q 。
+返回光线最先遇到的接收器的编号（保证光线最终会遇到一个接收器）。
+
+示例：
+输入： p = 2, q = 1
+输出： 2
+解释： 这条光线在第一次被反射回左边的墙时就遇到了接收器 2 。
+
+提示：
+1 <= p <= 1000
+0 <= q <= p
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/mirror-reflection
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+class Solution {
+public:
+	int mirrorReflection(int p, int q) {
+
+	}
+};
+#pragma endregion
+
 #pragma region 932_漂亮数组
 /*
 对于某些固定的 N，如果数组 A 是整数 1, 2, ..., N 组成的排列，使得：
