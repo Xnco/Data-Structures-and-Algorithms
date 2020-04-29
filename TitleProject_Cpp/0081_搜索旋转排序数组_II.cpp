4d551983ecedd9c8a6b70bd692f2434a381a7c61
Anthony_FU <xuanyylhky@163.com>
2020-04-29 11:23:54 +0800

    Update 0081_搜索旋转排序数组_II.cpp
    增加一个O(log n)的解法, 最坏情况也是O(n)

--- TitleProject_Cpp/0081_搜索旋转排序数组_II.cpp
+++ TitleProject_Cpp/0081_搜索旋转排序数组_II.cpp
@@ -37,5 +37,60 @@ public:
 		}
 		return false;
 	}
+
+	// 解法2 在33题的基础上增加一个去重复的循环即可
+	bool search2(vector<int>& nums, int target)
+	{
+		if (nums.size() == 0)
+		{
+			return fasle;
+		}
+
+		int left = 0;
+		int right = nums.size() - 1;
+		while (left <= right)
+		{
+			while (left != right && nums[left] == nums[right])
+			{
+				right--;// 有重复值的情况右值往左移动到不重复为止
+			}
+			int mid = (left + right) / 2;
+			if (nums[mid] == target)
+			{
+				return true;
+			}
+
+			// 要先判断左边是连续的还是右边是连续的
+			if (nums[mid] >= nums[0])
+			{
+				// 左边是连续的
+				if (target >= nums[left] && target < nums[mid]) // 判断target是否在左边
+				{
+					// in left 
+					right = mid - 1;
+				}
+				else
+				{
+					// in right
+					left = mid + 1;
+				}
+			}
+			else
+			{
+				// 右边是连续的
+				if (target <= nums[right] && target > nums[mid]) // 判断target是否在左边
+				{
+					// in right
+					left = mid + 1;
+				}
+				else
+				{
+					// in left 
+					right = mid - 1;
+				}
+			}
+		}
+		return fasle;
+	}
 };
 #pragma endregion
