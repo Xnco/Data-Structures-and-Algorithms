e2c370c4d94ee0231ae9af1172b06ada7543452c
Pastacup <xuanyylhky@163.com>
2020-10-01 14:23:41 +0800

    Add TitleProject_xcode proj

---
+++ TitleProject_xcode/TitleProject_xcode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TitleProject_xcode
+//
+//  Created by 付梅轩 on 2020/10/1.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
